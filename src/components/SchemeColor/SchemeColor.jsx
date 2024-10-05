a9e88ef6c1d797b9db62975999fd0901232d1a42
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-05 18:07:02 +0200

    component_SchemeColor

---
+++ src/components/SchemeColor/SchemeColor.jsx
@@ -0,0 +1,11 @@
+import './SchemeColor.css';
+
+const SchemeColor = (color) => {
+  return (
+    <>
+      <div className="scheme-color">{color}</div>
+    </>
+  );
+};
+
+export default SchemeColor;
